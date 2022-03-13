b3e0558ac5deb4119342c38b168ca3c94d50f21c
aanchal2520 <aanchal2520@gmail.com>
2022-03-13 21:54:16 +0530

    Logging newsArticles for debugging

--- src/components/News.js
+++ src/components/News.js
@@ -19,6 +19,7 @@ const News = (props) => {
             .then((data) => {
                 setProgress(70);
                 setNewsArticles(data.articles);
+                console.log(data.articles);
                 setProgress(100);
             })
     }
